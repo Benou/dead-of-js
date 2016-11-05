fa508df60f4f42a0cd0a6933c3a0f6474ec275ae
Benou <benou.lisik@gmail.com>
2016-11-05 17:09:24 +0100

    simple test for CI

--- src/app/app.component.spec.js
+++ src/app/app.component.spec.js
@@ -1,3 +1,22 @@
 /**
  * Created by Benou on 03/11/2016.
  */
+import angular from 'angular';
+import 'angular-mocks';
+
+import {AppComponent} from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(() => {
+    angular
+      .module('test', [])
+      .component('app', AppComponent);
+    angular.mock.module('test');
+  });
+
+  it('should render correctly', angular.mock.inject(($rootScope, $compile) => {
+    const element = $compile('<app></app>')($rootScope);
+    $rootScope.$digest();
+    expect(element).toBeDefined();
+  }));
+});
